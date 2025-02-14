eaed4e3e5a3fc9fd19f51fc64579a28c02a2279a
jeonghoon11 <jih361@naver.com>
2025-02-14 22:29:05 +0900

    FEAT: 리액트 라우터 추가 - 전체 컴포넌트 Router설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
 import { createRoot } from "react-dom/client";
 import GuideLine from "./components/GuideLine";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<GuideLine />);
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <GuideLine />
+  </BrowserRouter>
+);
