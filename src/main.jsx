2c15c550552423da568799f7ce16a33474456f76
jeonghoon11 <jih361@naver.com>
2025-02-18 00:49:17 +0900

    [feat] 개발 환경 재구축

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
-import GuideLine from "./components/GuideLine";
+import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <GuideLine />
+    <App />
   </BrowserRouter>
 );
