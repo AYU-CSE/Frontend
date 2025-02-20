65e556d885215c8ce87d34e2bc74363ab8672d8f
jeonghoon11 <jih361@naver.com>
2025-02-20 23:26:33 +0900

    [feat] header nav 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,64 @@
 import Logo from "../../assets/Anyang_University_CSE_Logo.png";
 import "./Header.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [isHover, setIsHover] = useState(false);
+
+  const handleIsHover = () => {
+    setIsHover(!isHover);
+  };
+
   return (
     <div className="Header">
       <img src={Logo} className="header_left"></img>
       <div className="header_center">
-        <li className="header_center_professorInfo">교수 소개</li>
-        <li className="header_center_departmentInfo">학과 소개</li>
-        <li className="header_center_lectureRoom">강의실</li>
-        <li className="header_center_notice">공지 사항</li>
-        <li className="header_center_community">커뮤니티</li>
+        <li>
+          <Link className="header_center_professorInfo" to={"/professor-Info"}>
+            교수 소개
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="header_center_departmentInfo"
+            to={"/department-Info"}
+          >
+            학과 소개
+          </Link>
+        </li>
+        <li>
+          <Link className="header_center_lectureRoom" to={"/student-Service"}>
+            강의실
+          </Link>
+        </li>
+        <li>
+          <Link className="header_center_notice" to={"/notices"}>
+            공지 사항
+          </Link>
+        </li>
+        <li>
+          <Link className="header_center_community" to={"/community"}>
+            커뮤니티
+          </Link>
+        </li>
       </div>
       <div className="header_right">
-        <li className="header_right_home">Home</li>
-        <li className="header_right_AYU">AYU</li>
-        <li className="header_right_Login">LOGIN</li>
+        <li>
+          <Link className="header_right_home" to={"/"}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="header_right_AYU" to={"https://www.anyang.ac.kr/"}>
+            AYU
+          </Link>
+        </li>
+        <li>
+          <Link className="header_right_Login" to={"/login"}>
+            LOGIN
+          </Link>
+        </li>
       </div>
     </div>
   );
