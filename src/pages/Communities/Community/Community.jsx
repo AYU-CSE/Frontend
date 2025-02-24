4817b5dd0e60ec4edcf6fdec0966914de216d06a
Kimgyuilli <rlarbdlf222@naver.com>
2025-02-24 22:57:17 +0900

    [feat] 자료실 html 틀만 구성

--- src/pages/Communities/Community/Community.jsx
+++ src/pages/Communities/Community/Community.jsx
@@ -1,6 +1,25 @@
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import "./Community.css";
+
 // 커뮤니티 (main)
 const Community = () => {
-  return <div>Community(main)</div>;
+  return (
+    <page>
+      <Header/>
+      <main className="community_main">
+        <header></header>
+        <search>
+
+          <div> 검색 카테고리 </div>
+          {/* 검색창 */}
+          <input type="text" /> 
+          <button>검색 버튼</button>
+        </search>
+      </main>
+      <Footer />
+    </page>
+  );
 };
 
 export default Community;
