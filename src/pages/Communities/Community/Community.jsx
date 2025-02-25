86013617218a2caaf7ba1d51b2ec8c8079d70e4f
Kimgyuilli <rlarbdlf222@naver.com>
2025-02-25 11:51:45 +0900

    [feat] - 커뮤니티 페이지 기본 css까지 구현 - NanumSquareNeo 폰트 여러가지 버전으로 추가

--- src/pages/Communities/Community/Community.jsx
+++ src/pages/Communities/Community/Community.jsx
@@ -2,23 +2,57 @@ import Footer from "../../../components/Footer/Footer";
 import Header from "../../../components/Header/Header";
 import "./Community.css";
 
-// 커뮤니티 (main)
+// 커뮤니티 (자료실)
 const Community = () => {
   return (
-    <page>
-      <Header/>
+    <div className="community">
+      <Header />
       <main className="community_main">
-        <header></header>
-        <search>
+        <header className="community_header">
+          <div>커뮤니티 &gt; <span>자료실</span></div>
+        </header>
 
-          <div> 검색 카테고리 </div>
-          {/* 검색창 */}
-          <input type="text" /> 
-          <button>검색 버튼</button>
-        </search>
+        {/* 검색창 */}
+        <div className="community_search">
+          <select className="category_select">
+            <option value="all">전체</option>
+            <option value="title">제목</option>
+            <option value="author">작성자</option>
+          </select>
+          <input type="text" placeholder="검색어를 입력해 주세요" />
+          <button className="search_button">검색</button>
+        </div>
+
+        {/* 자료실 테이블 */}
+        <section className="community_table_section">
+          <table className="community_table">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>제목</th>
+                <th>과목명</th>
+                <th>작성자</th>
+                <th>작성일</th>
+                <th>조회수</th>
+              </tr>
+            </thead>
+            <tbody>
+              {[...Array(5)].map((_, index) => (
+                <tr key={index}>
+                  <td>303</td>
+                  <td>SDL 소스코드 📝</td>
+                  <td>객체지향프로그래밍</td>
+                  <td>이상훈</td>
+                  <td>2025-02-15</td>
+                  <td>124</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
       </main>
       <Footer />
-    </page>
+    </div>
   );
 };
 
