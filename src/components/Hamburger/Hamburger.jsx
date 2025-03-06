837147420059bba9755d82a1fc4fcf24782d6596
Kimgyuilli <rlarbdlf222@naver.com>
2025-03-06 22:54:52 +0900

    [design] 드롭다운 추가

--- src/components/Hamburger/Hamburger.jsx
+++ src/components/Hamburger/Hamburger.jsx
@@ -1,14 +1,31 @@
 import "./Hamburger.css";
+import user from "../../assets/icons/user.svg";
+import notice from "../../assets/icons/notice.svg";
+import edit from "../../assets/icons/edit.svg";
 
 function Hamburger() {
   return (
-    <label for="hamburger" id="hamburger-icon">
-      <div className="hamburger_bar">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </label>
+    <div className="hamburger_container">
+      <label for="hamburger" id="hamburger_icon">
+        <div className="hamburger_dropdown">
+          <a className="hamburger_user">
+            <img src={user} alt="마이페이지" />
+          </a>
+          <a className="hamburger_notice">
+            <img src={notice} alt="알림" />
+          </a>
+          <a className="hamburger_writing">
+            <img src={edit} alt="글쓰기" />
+          </a>
+        </div>
+
+        <div className="hamburger_bar">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </label>
+    </div>
   );
 }
 
