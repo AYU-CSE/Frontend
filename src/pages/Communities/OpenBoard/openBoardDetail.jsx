a1b63c6aada4751f4a074124102e7248d5a68630
Kimgyuilli <rlarbdlf222@naver.com>
2025-03-08 02:40:57 +0900

    [feat] 오픈 커뮤니티 메인 컨텐츠 완성

--- src/pages/Communities/OpenBoard/openBoardDetail.jsx
+++ src/pages/Communities/OpenBoard/openBoardDetail.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import Footer from "../../../components/Footer/Footer";
 import Nav from "../../../components/Nav/Nav";
 import Hamburger from "../../../components/Hamburger/Hamburger";
 import updatedBoardData from "../openBoardData";
 
+import profileImage from "../../../assets/image/profile_temp.jpg";
+import bookmarkLine from "../../../assets/icons/bookmark-line.svg";
+import bookmarkFill from "../../../assets/icons/bookmark-fill.svg";
+
 import styles from "./openBoardDetail.module.css";
 
 const OpenBoardDetail = () => {
   const { id } = useParams();
   const post = updatedBoardData.find((item) => item.id === parseInt(id));
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const [isLiked, setIsLiked] = useState(false);
 
   if (!post) {
     return (
@@ -31,33 +39,52 @@ const OpenBoardDetail = () => {
 
   return (
     <div className={styles.community}>
-      <Header />
+      <Nav />
       <main className={styles.community_main}>
         <header className={styles.community_header}>
           <div>
             커뮤니티 &gt; <span>오픈게시판</span>
           </div>
         </header>
-
-        <section className={styles.open_community_detail}>
-          <header>
-            <div className={styles.open_community_detail_header_left}>
-              <span>No.{post.id}</span>
-              <span>조회수 {post.views}</span>
+        <div className={styles.post_container}>
+          <aside className={styles.detail_profile}>
+            <div className={styles.detail_profileImage}>
+              <img src={profileImage} alt="프로필 이미지" />
             </div>
-            <span>{post.date}</span>
-          </header>
-          <div className={styles.open_community_detail_title}>
-            <h2 className={styles.open_community_title}>{post.title}</h2>
-            <div className={styles.community_dibs}></div>
-          </div>
-          <pre className={styles.post_content}>{post.content}</pre>
-          <footer>
-            <span>좋아요♡</span>
-            <a>답글 달기</a>
-            <span>댓글 20</span>
-          </footer>
-        </section>
+            <span>{post.author}</span>
+          </aside>
+
+          <section className={styles.open_community_detail}>
+            <header>
+              <div className={styles.open_community_detail_header_left}>
+                <span>No.{post.id}</span>
+                <span>조회수 {post.views}</span>
+              </div>
+              <span>{post.date}</span>
+            </header>
+            <div className={styles.open_community_detail_title}>
+              <h2 className={styles.open_community_title}>{post.title}</h2>
+
+              <button
+                className={styles.community_dibs}
+                onClick={() => setIsBookmarked(!isBookmarked)}
+              >
+                <img
+                  src={isBookmarked ? bookmarkFill : bookmarkLine}
+                  alt="북마크 버튼"
+                />
+              </button>
+            </div>
+            <pre className={styles.post_content}>{post.content}</pre>
+            <footer>
+              <a onClick={() => setIsLiked(!isLiked)}>
+                {isLiked ? "❤️" : "♡"} 좋아요
+              </a>
+              <a>답글 달기</a>
+              <span>댓글 20</span>
+            </footer>
+          </section>
+        </div>
         <div className={styles.hr}></div>
 
         <Hamburger />
