e1517fdd132965b52c6ea7db4c15c5c5f97fb195
jeonghoon11 <jih361@naver.com>
2025-03-07 17:50:41 +0900

    [feat] 로그인 페이지 구현 - 기본 틀 구현 - nav 설정

--- src/route/routes/AppRoutes.jsx
+++ src/route/routes/AppRoutes.jsx
@@ -6,6 +6,7 @@ import Notices from "../../pages/Notices/Notices/Notices";
 import ProfessorInfo from "../../pages/Professors/ProfessorsInfo/ProfessorInfo";
 import StudentService from "../../pages/StudentServices/StudentService/StudentService";
 import Notfound from "../../pages/Notfound/Notfound";
+import LoginPage from "../../pages/Login/LoginPage";
 
 const AppRoutes = () => {
   return (
@@ -16,6 +17,7 @@ const AppRoutes = () => {
       <Route path="/notices" element={<Notices />} />
       <Route path="/professor-Info" element={<ProfessorInfo />} />
       <Route path="/student-Service" element={<StudentService />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/*" element={<Notfound />} />
     </Routes>
   );
