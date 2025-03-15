05ea7b70fbb618b8a4f4502dd8285ca9d8af6a5b
jeonghoon11 <jih361@naver.com>
2025-03-16 01:54:40 +0900

    [feat] nav바 어설픈 구현

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,11 +6,8 @@ import "./Nav.css";
 
 const Nav = ({ bgWhiteColor = false }) => {
   const [isHover, setIsHover] = useState(true);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [logo, setLogo] = useState(Logo1);
-  const [headerHeight, setHeaderHeight] = useState(110);
-  const minHeight = 70; // 축소된 최소 높이
-  const maxHeight = 150; // 기본 최대 높이
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleMouseEnter = () => {
     setIsHover(true);
@@ -18,30 +15,29 @@ const Nav = ({ bgWhiteColor = false }) => {
   };
 
   const handleMouseLeave = () => {
+    if (window.scrollY === 0) return;
     setIsHover(false);
     setLogo(Logo2);
-    setIsMenuOpen(false);
+    setIsDropdownOpen(false);
   };
 
-  const handleCategoryEnter = () => {
-    setIsMenuOpen(true);
+  const handleDropdownEnter = () => {
+    setIsDropdownOpen(true);
   };
+
+  // const handleDropdownLeave = () => {
+  //   setIsDropdownOpen(false);
+  // };
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const newHeight = Math.max(
-        maxHeight - (scrollY / 30) * (maxHeight - minHeight),
-        minHeight
-      );
-      setHeaderHeight(newHeight);
-
-      if (scrollY > 30) {
+      if (window.scrollY > 50) {
         setIsHover(false);
         setLogo(Logo2);
       } else {
         setIsHover(true);
         setLogo(Logo1);
+        setIsDropdownOpen(false);
       }
     };
 
@@ -50,86 +46,40 @@ const Nav = ({ bgWhiteColor = false }) => {
   }, []);
 
   return (
-    <div className={`nav-container ${isMenuOpen ? "blur-background" : ""}`}>
+    <div className="nav-container">
       <div
-        className={`Nav ${isHover ? "Nav_hover" : "Nav_shrink"}
-      ${bgWhiteColor ? " bgWhite" : "var(--color-main)"} `}
+        className={`Nav 
+          ${isHover ? "Nav_hover" : "Nav_shrink"} 
+          ${bgWhiteColor ? " bgWhite" : "var(--color-main)"}`}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        // // 스크롤 옵션 적용
-        // style={{
-        //   height: `${headerHeight}px`,
-        // }}
       >
         <Link to={"/"} className="nav_left">
           <img src={logo} alt="Logo"></img>
         </Link>
-        <ul className="nav_center">
-          <li onMouseEnter={handleCategoryEnter}>
+
+        <ul
+          className="nav_center"
+          onMouseEnter={handleDropdownEnter}
+          // onMouseLeave={handleDropdownLeave}
+        >
+          <li>
             <Link to={"/professor-Info"}>교수 소개</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to={"/professor-Info"}>교수진</Link>
-              </li>
-              <li>
-                <Link to={"/past-professor"}>역대 교수</Link>
-              </li>
-            </ul>
           </li>
-          <li onMouseEnter={handleCategoryEnter}>
+          <li>
             <Link to={"/department-Info"}>학과 소개</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to={"/department"}>학과 안내</Link>
-              </li>
-              <li>
-                <Link to={"/curriculum"}>교과목 소개</Link>
-              </li>
-              <li>
-                <Link to={"/lab"}>LAB</Link>
-              </li>
-            </ul>
           </li>
-          <li onMouseEnter={handleCategoryEnter}>
+          <li>
             <Link to={"/student-Service"}>학생 서비스</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to={"/lecture-room"}>강의실</Link>
-              </li>
-              <li>
-                <Link to={"/study-room"}>열람실 및 스터디룸</Link>
-              </li>
-            </ul>
           </li>
-          <li onMouseEnter={handleCategoryEnter}>
+          <li>
             <Link to={"/notices"}>공지</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to={"/announcement"}>공지사항</Link>
-              </li>
-              <li>
-                <Link to={"/events"}>행사</Link>
-              </li>
-              <li>
-                <Link to={"/academic-calendar"}>학사일정</Link>
-              </li>
-              <li>
-                <Link to={"/internship"}>인턴</Link>
-              </li>
-            </ul>
           </li>
-          <li onMouseEnter={handleCategoryEnter}>
+          <li>
             <Link to={"/open-board"}>커뮤니티</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to={"/open-board"}>오픈 게시판</Link>
-              </li>
-              <li>
-                <Link to={"/resources"}>자료실</Link>
-              </li>
-            </ul>
           </li>
         </ul>
+
         <ul className="nav_right">
           <li>
             <Link to={"/"} className="nav_right_home">
@@ -148,6 +98,64 @@ const Nav = ({ bgWhiteColor = false }) => {
           </li>
         </ul>
       </div>
+
+      {isDropdownOpen && (
+        <div
+          className={`dropdown_container ${
+            isDropdownOpen ? "dropdown_visible" : ""
+          }`}
+        >
+          <ul>
+            <li>
+              <Link to={"/professor-Info"}>교수진</Link>
+            </li>
+            <li>
+              <Link to={"/past-professor"}>역대 교수</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to={"/department"}>학과 안내</Link>
+            </li>
+            <li>
+              <Link to={"/curriculum"}>교과목 소개</Link>
+            </li>
+            <li>
+              <Link to={"/lab"}>LAB</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to={"/lecture-room"}>강의실</Link>
+            </li>
+            <li>
+              <Link to={"/study-room"}>열람실 및 스터디룸</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to={"/announcement"}>공지사항</Link>
+            </li>
+            <li>
+              <Link to={"/events"}>행사</Link>
+            </li>
+            <li>
+              <Link to={"/academic-calendar"}>학사일정</Link>
+            </li>
+            <li>
+              <Link to={"/internship"}>인턴</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to={"/open-board"}>오픈 게시판</Link>
+            </li>
+            <li>
+              <Link to={"/resources"}>자료실</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
