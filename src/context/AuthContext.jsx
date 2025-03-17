1f9247916f610eaae14a774f8dcf349a7cc83abd
jeonghoon11 <jih361@naver.com>
2025-03-17 22:55:16 +0900

    [feat] 전체 페이지 기본 상태 Logout상태로 지정

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ export const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
   //props를 하나라도 넣어야 route설정해놓은 것들이 모두 렌더링 된다.
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
 
   return (
     <AuthContext.Provider value={{ isLogin }}>{children}</AuthContext.Provider>
