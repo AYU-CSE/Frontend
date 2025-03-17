c128f77e6b5d43a4feeed9fd9b4401274c053a3b
winchoose <tmdxor741@naver.com>
2025-03-17 17:26:15 +0900

    [Feat] 학과소개페이지 구현, 개인 components 이동

--- src/pages/Departments/DepartmentInfo/DepartmentInfo.jsx
+++ src/pages/Departments/DepartmentInfo/DepartmentInfo.jsx
@@ -1,6 +1,22 @@
 // 학과 소개 (main)
+import Footer from "../../../components/Footer/Footer";
+import Nav from "../../../components/Nav/Nav";
+import Department from "../Department/Department";
+import "./DepartmentInfo.css"
+
 const DepartmentInfo = () => {
-  return <div>DepartmentInfo (main) </div>;
+  return (
+    <div className="DepartmentInfo">
+      <Nav/>
+      <div className="Text">
+      <p style={{ display: "inline-block", fontSize: "30px", fontWeight: "bold" }}>학과 소개</p>
+      <p style={{ display: "inline-block", margin: "0 10px", fontWeight: "bold", fontSize: "30px" }}>&gt;</p>
+      <p style={{ display: "inline-block", fontSize: "20px", fontWeight: "bold" }}>학과 안내</p>
+      </div>
+      <Department/>
+      <Footer/>
+    </div>
+  )
 };
 
 export default DepartmentInfo;
