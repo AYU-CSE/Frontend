a588789fed299d786534b37d328375c3b3fff476
Kimgyuilli <rlarbdlf222@naver.com>
2025-03-19 12:05:25 +0900

    [feat] 게시글 작성 기능 구현

--- src/components/Editor/TinyEditor.jsx
+++ src/components/Editor/TinyEditor.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 const apiKey = import.meta.env.VITE_TINYMCE_API_KEY || "no-api-key";
 
-export default function TinyEditor() {
+export default function TinyEditor({ onEditorChange }) {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    const fixPassiveEventListener = (event) => {};
+
+    document.addEventListener("touchmove", fixPassiveEventListener, {
+      passive: true,
+    });
+
+    return () => {
+      document.removeEventListener("touchmove", fixPassiveEventListener);
+    };
+  }, []);
+
   return (
     <Editor
       apiKey={apiKey}
+      value={content}
+      onEditorChange={(newContent) => {
+        setContent(newContent);
+        if (onEditorChange) {
+          onEditorChange(newContent);
+        }
+      }}
       init={{
         menubar: false,
+        theme: "silver",
         plugins: [
           // Core editing features
           "anchor",
@@ -26,15 +48,15 @@ export default function TinyEditor() {
           "wordcount",
         ],
         toolbar:
-          "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog typography | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat",
-        tinycomments_mode: "embedded",
-        tinycomments_author: "Author name",
+          "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | addcomment showcomments | spellcheckdialog typography | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat",
+
+        // 이건 뭔지 모르겠음 일단 놔둠둠
         mergetags_list: [
           { value: "First.Name", title: "First Name" },
           { value: "Email", title: "Email" },
         ],
       }}
-      initialValue="Welcome to TinyMCE!"
+      initialValue=""
     />
   );
 }
--- src/pages/Communities/EditPost/EditPost.jsx
+++ src/pages/Communities/EditPost/EditPost.jsx
@@ -1,8 +1,69 @@
+import { useState } from "react";
+
 import TinyMCEEditor from "../../../components/Editor/TinyEditor";
+import Footer from "../../../components/Footer/Footer";
+import Nav from "../../../components/Nav/Nav";
+
+import styles from "./EditPost.module.css";
+
 function EditPost() {
+  const [category, setCategory] = useState("전체 게시판");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const categories = [
+    "전체 게시판",
+    "학업 게시판",
+    "취업 게시판",
+    "프로젝트 게시판",
+    "정보 게시판",
+  ];
+
+  const handleSubmit = () => {
+    console.log("category:", category);
+    console.log("title:", title);
+    console.log("content:", content);
+  };
+
   return (
     <div>
-      <TinyMCEEditor />
+      <Nav />
+      <div className={styles.editPage}>
+        <header className={styles.editorHeader}>
+          <span>게시글 작성</span>
+          <button onClick={handleSubmit}>등록</button>{" "}
+        </header>
+        <div className={styles.divider}></div>
+        <section className={styles.contentContainer}>
+          <div className={styles.contentLeft}>
+            <header className={styles.contentHeader}>
+              <select
+                className={styles.postCategory}
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                {categories.map((cat) => (
+                  <option key={cat} value={cat}>
+                    {cat}
+                  </option>
+                ))}
+              </select>
+
+              <input
+                className={styles.postTitle}
+                type="text"
+                placeholder="제목을 입력해 주세요."
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </header>
+
+            <TinyMCEEditor onEditorChange={setContent} />
+          </div>
+          <div className={styles.contentRight}></div>
+        </section>
+      </div>
+      <Footer />
     </div>
   );
 }
