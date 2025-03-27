6c2a423ce4b1158aafd0f042b0f20d07c3dd1c61
Hui._. <yeonhee040512@gmail.com>
2025-03-27 18:15:54 +0900

    [feat] 스터디룸 및 열람실 페이지 구현 - 스터디룸 예약 페이지 구현 - 예약 페이지 링크 임시 연결

--- src/pages/StudentServices/StudyRoom/StudyRoom.jsx
+++ src/pages/StudentServices/StudyRoom/StudyRoom.jsx
@@ -1,20 +1,39 @@
-// 스터디룸
 import { useState, useEffect } from "react";
-
-import styles from "../studentService.module.css";
+import styles from "./StudyRoom.module.css";
 import Nav from "../../../components/Nav/Nav";
+import Footer from "../../../components/Footer/Footer";
+import Room from "./Room";
+import StudyRoomData from "./StudyRoomData";
+
+// 이미지 파일 import
+import SelfStudyRoom1 from "../../../../src/assets/image/StudyRooming/SelfStudyRoom1.png";
+import SelfStudyRoom2 from "../../../../src/assets/image/StudyRooming/SelfStudyRoom1.png";
+
+// 이미지 매핑
+const roomImages = {
+  "SelfStudyRoom1.png": SelfStudyRoom1,
+  "SelfStudyRoom1.png": SelfStudyRoom2,
+};
 
 const StudyRoom = () => {
   return (
     <div>
-      <Nav /> 
+      <Nav />
       <main className={styles.studentService_main}>
         <header className={styles.studentService_header}>
           <div>
             학생 서비스 &gt; <span>스터디룸 및 열람실</span>
           </div>
         </header>
+        <div className={styles.studyroomBody}>
+          <div className={styles.studyroom}>
+            {StudyRoomData.map((room) => (
+              <Room key={room.id} name={room.name} image={roomImages[room.image]} pos={room.pos} sit={room.sit} />
+            ))}
+          </div>
+        </div>
       </main>
+      <Footer />
     </div>
   );
 };
---
+++ src/pages/StudentServices/StudyRoom/StudyRoomData.js
@@ -0,0 +1,8 @@
+const StudyRoomData = [
+    { id: 1, name: "Self 학습실 1", image: "SelfStudyRoom1.png", pos: "아리관 3층", sit: "6석"},
+    { id: 2, name: "Self 학습실 2", image: "SelfStudyRoom1.png", pos: "아리관 3층", sit: "6석"},
+    { id: 3, name: "Self 학습실 3", image: "SelfStudyRoom1.png", pos: "아리관 3층", sit: "6석"},
+    { id: 4, name: "Self 학습실 4", image: "SelfStudyRoom1.png", pos: "아리관 3층", sit: "6석"},
+];
+
+export default StudyRoomData;
